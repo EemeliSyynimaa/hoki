ca46f9ccc523fb3ee1ebad2ea80b1b475fa984e8
Eemeli Syynimaa <EemeliSyynimaa@users.noreply.github.com>
2017-04-08 14:40:13 +0300

    Create window

--- source/win32_main.cpp
+++ source/win32_main.cpp
@@ -1,5 +1,42 @@
 #include <windows.h>
 
+int running = 0;
+
+LRESULT CALLBACK MainWindowProc(
+	HWND hwnd,
+	UINT uMsg,
+	WPARAM wParam,
+	LPARAM lParam)
+{
+	switch (uMsg)
+	{
+		case WM_CLOSE:
+		{
+			running = 0;
+		} break;
+
+		case WM_DESTROY:
+		{
+			running = 0;
+		} break;
+
+		case WM_KEYDOWN:
+		{
+			if (wParam == VK_ESCAPE)
+			{
+				running = 0;
+			}
+		} break;
+
+		default:
+		{
+			return DefWindowProcW(hwnd, uMsg, wParam, lParam);
+		}
+	}
+
+	return 0;
+}
+
 int CALLBACK WinMain(
     HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
@@ -11,8 +48,51 @@ int CALLBACK WinMain(
     (void)lpCmdLine;
     (void)nCmdShow;
 
-	MessageBoxA(0, "Is this cool or not? OK OK!", "Test window", MB_OK|MB_ICONINFORMATION);
+	WNDCLASSEXW wdx = { 0 };
 
-    return 0;
+	wdx.cbSize = sizeof(wdx);
+	wdx.style = CS_HREDRAW | CS_VREDRAW;
+	wdx.lpfnWndProc = MainWindowProc;
+	wdx.hInstance = hInstance;
+	wdx.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wdx.lpszClassName = L"HokiWindowClass";
+
+	RegisterClassExW(&wdx);
+
+	HWND handle = CreateWindowExW(
+			0,
+			wdx.lpszClassName,
+			L"Hoki",
+			WS_VISIBLE | WS_OVERLAPPEDWINDOW,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			0,
+			0,
+			hInstance,
+			0
+	);
+
+	running = 1;
+
+	while (running)
+	{
+		MSG msg;
+
+		while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+			{
+				running = 0;
+			}
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+	}
+
+	return 0;
 }
 
