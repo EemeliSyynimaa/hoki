24a9ad1585d49b41281762cb3c8df8cbfb6f80ac
Eemeli Syynimaa <EemeliSyynimaa@users.noreply.github.com>
2017-04-23 19:13:31 +0300

    Render hello triangle with OpenGL

--- source/win32_main.c
+++ source/win32_main.c
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include <gl/GL.h>
 
 int running = 0;
 
@@ -8,8 +9,14 @@ LRESULT CALLBACK MainWindowProc(
     WPARAM wParam,
     LPARAM lParam)
 {
+    static PAINTSTRUCT ps = { 0 };
+
     switch (uMsg)
     {
+        case WM_CREATE:
+        {
+        } break;
+
         case WM_CLOSE:
         {
             running = 0;
@@ -17,7 +24,25 @@ LRESULT CALLBACK MainWindowProc(
 
         case WM_DESTROY:
         {
+
             running = 0;
+        } break;
+
+        case WM_PAINT:
+        {
+            glClear(GL_COLOR_BUFFER_BIT);
+            glBegin(GL_TRIANGLES);
+            glColor3f(1.0f, 0.0f, 0.0f);
+            glVertex2i(0, 1);
+            glColor3f(0.0f, 1.0f, 0.0f);
+            glVertex2i(-1, -1);
+            glColor3f(0.0f, 0.0f, 1.0f);
+            glVertex2i(1, -1);
+            glEnd();
+            glFlush();
+
+            BeginPaint(hwnd, &ps);
+            EndPaint(hwnd, &ps);
         } break;
 
         case WM_KEYDOWN:
@@ -43,12 +68,15 @@ int CALLBACK WinMain(
     LPSTR lpCmdLine,
     int nCmdShow)
 {
-    (void)hInstance;
+    int pf = 0;
+    HWND hwnd = 0;
+    HDC hdc = 0;
+    HGLRC hrc = 0;
+    WNDCLASSEXW wdx = { 0 };
+    PIXELFORMATDESCRIPTOR pfd = { 0 };
+
     (void)hPrevInstance;
     (void)lpCmdLine;
-    (void)nCmdShow;
-
-    WNDCLASSEXW wdx = { 0 };
 
     wdx.cbSize = sizeof(wdx);
     wdx.style = CS_HREDRAW | CS_VREDRAW;
@@ -59,20 +87,39 @@ int CALLBACK WinMain(
 
     RegisterClassExW(&wdx);
 
-    CreateWindowExW(
-            0,
-            wdx.lpszClassName,
-            L"Hoki",
-            WS_VISIBLE | WS_OVERLAPPEDWINDOW,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            0,
-            0,
-            hInstance,
-            0
+    hwnd = CreateWindowExW(
+        0,
+        wdx.lpszClassName,
+        L"Hoki",
+        WS_VISIBLE | WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        0,
+        0,
+        hInstance,
+        0
     );
+
+    hdc = GetDC(hwnd);
+
+    pfd.nSize = sizeof(pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 32;
+
+    pf = ChoosePixelFormat(hdc, &pfd);
+
+    SetPixelFormat(hdc, pf, &pfd);
+
+    DescribePixelFormat(hdc, pf, sizeof(PIXELFORMATDESCRIPTOR), &pfd);
+
+    hrc = wglCreateContext(hdc);
+    wglMakeCurrent(hdc, hrc);
+
+    ShowWindow(hwnd, nCmdShow);
 
     running = 1;
 
@@ -90,8 +137,13 @@ int CALLBACK WinMain(
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
-
     }
+
+    wglMakeCurrent(0, 0);
+    wglDeleteContext(hrc);
+
+    ReleaseDC(hwnd, hdc);
+    DestroyWindow(hwnd);
 
     return 0;
 }
